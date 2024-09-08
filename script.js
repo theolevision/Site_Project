c1ed401937de64c3eff1650deb968d0619998187
Theo Penha Valente <theopvalente@gmail.com>
2024-09-08 14:35:36 -0300

    --

--- script.js
+++ script.js
@@ -22,7 +22,9 @@ const codes = [
     `let nome = "Maria";\nlet idade = 25;\nconsole.log(nome, idade);\n// Saída: Maria 25`,
     `const pi = 3.14159;\nconsole.log(pi);\n// Saída: 3.1415`,
     `let contador = 0;\ncontador = contador + 1;\nconsole.log(contador);\n// Saída: 1`,
-    `let arr = [1, 2, 3, 4];\narr.forEach(num => console.log(num));`
+    `var cidade = "Brasília";\nlet estado = "Distrito Federal";\nconsole.log("Cidade:", cidade, \n"Estado:", estado);`,
+    `let idade = 18;\nif (idade < 18) {\nconsole.log("Menor de idade");\n} else {\n console.log("Maior de idade");\n}
+`
 ];
 
 let currentIndex = 0;
@@ -34,7 +36,7 @@ function updateCode() {
     currentIndex = (currentIndex + 1) % codes.length;
 }
 
-setInterval(updateCode, 2000); // Troca o código a cada 3 segundos
+setInterval(updateCode, 1500); // Troca o código a cada 3 segundos
 
 // Inicializa com o primeiro código
 updateCode();
